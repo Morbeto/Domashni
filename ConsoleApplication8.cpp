211d793c06473bfc10cd0e2d2884c26e9318ec04
Moberto <115207699+Morbeto@users.noreply.github.com>
2022-10-23 14:46:06 +0300

    Add files via upload

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,62 @@
+// ConsoleApplication8.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    /*Task8
+    int a, b, c;
+      cout << "Enter the values of a ,b and c" << endl;
+      cin >> a >> b >> c;
+
+      if (a == 0 ) {
+          cout << "Error";
+      }
+
+      double X1, X2;
+      X1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+      X2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+      cout << "X1 is equal to " << X1 << " and X2 is equal to " << X2; */
+      /*Task9
+      double euro;
+      float lev;
+      cout << "Enter euro : ";
+      cin >> euro;
+      if (euro == 0.0) {
+          cout << "Error";
+      }
+      lev = euro * 1.95583;
+      cout << "That's equal to " << lev << endl;
+      return 0;                                                        */
+    /*Task10
+    double beer, rakia, iceCubes;
+    cin >> beer >> rakia >> iceCubes;
+    if (beer >= 1 || rakia >= 1 && iceCubes >= 1) {
+        cout << "Tishko drinks at home ";
+    }
+    else {
+        cout << "Tishko goes to the store";
+    }                                                                  */
+    
+    
+    /*Task11 
+    int n , OverallLenght;
+    cin >> n;
+    OverallLenght = n * (n + 1);
+    cout << "Tishko has travelled " << OverallLenght << "meters";
+    */
+    } 
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
